3428b936b2845ee0981659eeddb55c476432ee46
Prasannadeep Das <38263680+prasannadeep27@users.noreply.github.com>
2019-03-24 20:42:39 +0530

    Add files via upload

--- Assets/main.js
+++ Assets/main.js
@@ -1,11 +1,11 @@
 let active = 0;
 $(window).scroll(function (event) {
     let scroll = $(window).scrollTop();
-    if(scroll>=80 && !active){
+    if(scroll>=40 && !active){
       active=1;
       $(".fixed-nav").css({'display':'unset'}).animate({opacity:'1'},300);
     }
-    else if(scroll<80 && active){
+    else if(scroll<40 && active){
       active=0;
       $(".fixed-nav").animate({opacity:'0'},300).css({'display':'none'});
     }
